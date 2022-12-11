59931aea1b8e137f566395002d4e2ea2275da983
pgm-joripeti <joris.petillion@student.arteveldehs.be>
2022-12-11 21:15:55 +0100

    finetuned ahref and ahref:hover behaviours on index, header and footer, in line with example

--- static/js/index.js
+++ static/js/index.js
@@ -1,45 +1,33 @@
 (() => {
-
     const app = {
-
         init() {
             console.log(1);
             this.cache();
         },
-
         cache() {
             console.log(1);
             this.$indexpics = document.querySelector('.indexpics');
             this.generate();
         },
-
         generate() {
             console.log(1);
             this.$indexpics.innerHTML = this.generateHTMLForIndexPics();
         },
-
         generateHTMLForIndexPics() {
             return indexpics.map((indexpic) => {
                 return `
-                
-                
-                                <figure>
-                                    <img src=${indexpic.image} loading="lazy">
-                                    <figcaption>
-                                        <div class="indexpic-title"><span>${indexpic.description}</span></div>
-                                    </figcaption>
-                                </figure>
-                            
-                
+                    <figure>
+                        <a href="#"><img src=${indexpic.image} loading="lazy"></a>
+                        <figcaption>
+                            <div class="indexpic-title"><span>${indexpic.description}</span></div>
+                        </figcaption>
+                    </figure>
                 `
             }).join('');
-            
-            
             `
             ${indexpics.image}
             `
         },
-
 
     }; app.init();
 
